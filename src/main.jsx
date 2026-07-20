3e1d07b6f5527a2552f34877af457951e762a3bc
Lovepreet <294700247+lovepreetlove663-star@users.noreply.github.com>
2026-07-20 09:27:19 +0530

    Add new images and implement footer component; refactor App structure and styles
    - Added multiple image files for gallery and social media icons.
    - Created a new Footer component with contact information and social links.
    - Refactored App component to include About, Gallery, and Contact pages.
    - Removed unused CSS and integrated Tailwind CSS for styling.
    - Updated Contact and About pages with new layouts and features.
    - Implemented responsive design adjustments across various components.

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,54 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
-
-createRoot(document.getElementById('root')).render(
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import About from "./pages/About.jsx";
+import Gallery from "./pages/Gallery.jsx";
+import Contact from "./pages/Contact.jsx";
+import "./App.css"
+import Footer from "./components/Footer"
+createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <App />
-  </StrictMode>,
-)
+       <About />    
+      <Gallery />   
+     <Contact />
+     <Footer />
+  </StrictMode>
+);
+
+// import { StrictMode } from "react";
+// import { createRoot } from "react-dom/client";
+// import {
+//   createBrowserRouter,
+//   RouterProvider,
+// } from "react-router-dom";
+
+// import About from "./pages/About";
+// import Gallery from "./pages/Gallery";
+// import Contact from "./pages/Contact";
+// import Home from "./pages/Home";
+
+// import "./App.css";
+
+// const router = createBrowserRouter([
+//   // {
+//   //   path: "/",
+//   //   element: <Home />,
+//   // },
+//   {
+//     path: "/",
+//     element: <About />,
+//   },
+//   {
+//     path: "/gallery",
+//     element: <Gallery />,
+//   },
+//   {
+//     path: "/contact",
+//     element: <Contact />,
+//   },
+// ]);
+
+// createRoot(document.getElementById("root")).render(
+//   <StrictMode>
+//     <RouterProvider router={router} />
+//   </StrictMode>
+// );
